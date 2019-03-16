4afde32278770ae00a461e607759f588ac84fa4d
delieva <delievaalexandra@gmail.com>
2019-03-16 10:51:57 +0200

    with pipe

--- serverHPCH.js
+++ serverHPCH.js
@@ -2,98 +2,24 @@ const util = require('util');
 const http = require('http');
 const url = require('url');
 
-function notFound(res){
-	res.writeHead(404, 'text/plain');
-	res.end('404: Not found');
-}
 
-http.createServer(function(req, res){
-	console.log(req);
+http.createServer(function(clientReq, clientRes){
+	console.log(clientReq);
+	console.log(clientReq.headers.host);
 	
 	
 	
 	const options = {
-		host: req.headers.host,
-		// headers: req.headers,
-		port: 80,
-		method: req.method
+		host: clientReq.headers.host,
+		url: clientReq.url,
+		method: clientReq.method
 	};
 	
-	const p_req = http.request(options, res1 => {
-		console.log('\nhello honey ');
-		// res1.on('data', (chunk) => {
-		// 	res.write(chunk);
-		// 	console.log("hi, kitty")
-		// });
-		// res1.on('end', () => {
-		// 	res.end();
-		// })
-		res1.pipe(res);
+	const proxyReq = http.request(options, proxyRes => {
+		proxyRes.headers=clientRes.headers;
+		proxyRes.pipe(clientRes);
 	});
-	p_req.end();
+	proxyReq.end();
 }).listen(8080);
-
-
-
-
-
-
-//
-//
-// http.createServer(onRequest).listen(3000);
-//
-// function onRequest(client_req, client_res) {
-// 	console.log('serve: ' + client_req.url);
-//
-// 	var options = {
-// 		hostname: 'www.google.com',
-// 		port: 80,
-// 		path: client_req.url,
-// 		method: client_req.method,
-// 		headers: client_req.headers
-// 	};
-//
-// 	var proxy = http.request(options, function (res) {
-// 		client_res.writeHead(res.statusCode, res.headers);
-// 		res.pipe(client_res, {
-// 			end: true
-// 		});
-// 	});
-//
-// 	client_req.pipe(proxy, {
-// 		end: true
-// 	});
-// }
-
-
-// const http = require('http');
-// const request = require('request');
-//
-// http.createServer((req, res) => {
-// 	console.log(req);
-//
-// 	if (req.method === 'GET'){
-// 		request({
-// 			method: req.method,
-// 			url: req.url,
-// 			headers: req.headers
-// 		}, (err, resp, body) => {
-// 			res.end(body)
-// 		})
-// 	}
-//
-// 	console.log(req.body);
-// 	if (req.method === 'POST'){
-// 		request({
-// 			method: req.method,
-// 			url: req.url,
-// 			headers: req.headers,
-// 			form: req.form,
-// 		}, (err, resp, body) => {
-// 			res.end(body)
-// 		})
-// 	}
-//
-// }).listen(8080);
 
 
