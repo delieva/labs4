3ffdf8795b5bfc83b4e340481868ee01ff484822
delieva <delievaalexandra@gmail.com>
2019-03-16 10:26:28 +0200

    with pipe

--- serverHPCH.js
+++ serverHPCH.js
@@ -7,47 +7,30 @@ function notFound(res){
 	res.end('404: Not found');
 }
 
-http.createServer(function(b_req, b_res){
-	let b_url = url.parse(b_req.url, true);
-
-	if(!b_url.query || !b_url.query.url){
-		return notFound(b_res)
-	}
-
-	let p_url = url.parse(b_url.query.url)
+http.createServer(function(req, res){
+	console.log(req);
 	
-	//console.log(b_req.url);
+	
 	
 	const options = {
-		host: p_url.hostname,
+		host: req.headers.host,
+		// headers: req.headers,
 		port: 80,
-		method: b_req.method
-	}
-	//console.log(options);
+		method: req.method
+	};
 	
 	const p_req = http.request(options, res1 => {
 		console.log('\nhello honey ');
+		// res1.on('data', (chunk) => {
+		// 	res.write(chunk);
+		// 	console.log("hi, kitty")
+		// });
+		// res1.on('end', () => {
+		// 	res.end();
+		// })
+		res1.pipe(res);
 	});
 	p_req.end();
-	
-	p_req.addListener('response', (p_res) => {
-		b_res.writeHead(b_res.statusCode, p_res.headers);
-		p_res.on('data', (chunk) => {
-			b_res.write(chunk);
-		});
-
-		p_res.on('end', () => {
-			b_res.end();
-		})
-		// p_req.addListener("data", (chunk) => {
-		// 	b_res.write(chunk);
-		// 	console.log("FUCK");
-		// });
-		//
-		// p_req.addListener("end", function() {
-		// 	b_res.end();
-		// });
-	})
 }).listen(8080);
 
 
