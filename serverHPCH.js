53b4ed9a6be1783e40d5010480e3e8275c0ad927
delieva <delievaalexandra@gmail.com>
2019-03-17 15:32:05 +0200

    env1

--- serverHPCH.js
+++ serverHPCH.js
@@ -1,23 +1,21 @@
-const util = require('util');
 const http = require('http');
 const url = require('url');
 
 
 http.createServer(function(clientReq, clientRes){
-	// console.log(clientReq);
+	const clientUrl = url.parse(clientReq.url);
 	console.log(clientReq.url);
 	
-	
-	
 	const options = {
+		hostname: clientUrl.host,
+		path: clientUrl.path,
 		method: clientReq.method
 	};
 	
-	const proxyReq = http.request(clientReq.url, proxyRes => {
+	const proxyReq = http.request(options, (proxyRes) => {
+		proxyRes.url = clientUrl.href;
 		proxyRes.headers=clientRes.headers;
 		proxyRes.pipe(clientRes);
 	});
 	proxyReq.end();
 }).listen(8080);
-
-
