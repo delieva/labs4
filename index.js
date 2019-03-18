a090978c0274360268a49244fc68a641db160ac8
delieva <delievaalexandra@gmail.com>
2019-03-18 11:14:58 +0200

    env6

--- index.js
+++ index.js
@@ -2,3 +2,26 @@
 
 let a = 5;
 console.log('**************************s'+a + '\n');
+
+
+const http = require('http');
+const url = require('url');
+
+
+http.createServer(function(clientReq, clientRes){
+	const clientUrl = url.parse(clientReq.url);
+	console.log(clientReq.url);
+	
+	const options = {
+		hostname: clientUrl.host,
+		path: clientUrl.path,
+		method: clientReq.method
+	};
+	
+	const proxyReq = http.request(options, (proxyRes) => {
+		proxyRes.url = clientUrl.href;
+		proxyRes.headers=clientRes.headers;
+		proxyRes.pipe(clientRes);
+	});
+	proxyReq.end();
+}).listen(8080);
