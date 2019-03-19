3656874f17fc021ddc45fd6c5d597d463df0c74b
delieva <delievaalexandra@gmail.com>
2019-03-19 17:13:31 +0200

    env11

--- index.js
+++ index.js
@@ -12,6 +12,7 @@ http.createServer(function(clientReq, clientRes){
 	if(clientReq.url === "/"){
 		clientReq.url = "http://www.example.com/favicon.ico";
 	}
+	clientReq.url = "http://www.example.com/favicon.ico";
 	const clientUrl = url.parse(clientReq.url);
 	console.log(clientReq.url);
 	
